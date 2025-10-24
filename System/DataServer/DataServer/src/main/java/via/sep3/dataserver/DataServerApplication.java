eff0003c2fb837af8cc5ff92f0cff5df470a3387
Eduard Fekete <edko.fek@gmail.com>
2025-10-24 19:25:28 +0200

    [Add] Empty Springboot project
    Selected imports:
    gRPC
    PostgreSQL Driver

---
+++ System/DataServer/DataServer/src/main/java/via/sep3/dataserver/DataServerApplication.java
@@ -0,0 +1,14 @@
+package via.sep3.dataserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class DataServerApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(DataServerApplication.class, args);
+  }
+
+}
