085ab497b596e12d6b7491ad454a656c3eaddcd8
Piotr Junosz <piotrju2005@wp.pl>
2025-10-29 10:35:45 +0100

    [update] simple spring boot app - added controller and entity classes

---
+++ System/DataServer/DataServer/src/main/java/via/sep3/dataserver/Language.java
@@ -0,0 +1,37 @@
+package via.sep3.dataserver;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import javax.annotation.processing.Generated;
+
+@Entity
+public class Language
+{
+  @Id
+  private String code;
+
+  private String name;
+
+  public String getCode()
+  {
+    return code;
+  }
+
+  public void setCode(String code)
+  {
+    this.code = code;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+}
