7416b15ef104affbb3dee1da8dc1560bebdd7eef
Eduard Fekete <edko.fek@gmail.com>
2025-11-04 21:23:45 +0100

    [Refactor] Client app entities

--- System/ClientApp/BlazorApp/Program.cs
+++ System/ClientApp/BlazorApp/Program.cs
@@ -7,18 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddHttpClient<ICourseService, HttpCourseService>(c => c.BaseAddress = new Uri("http://localhost:5161"));
+var hostUri = builder.Configuration["HostUri"] ?? "http://localhost:5161";
+
+builder.Services.AddHttpClient<ICourseService, HttpCourseService>(c => c.BaseAddress = new Uri(hostUri));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForErrors: true);
 
 app.UseHttpsRedirection();
 
