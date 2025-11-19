590df316d22173a0a41f5b919875f2c1e09d6ed1
Eduard Fekete <edko.fek@gmail.com>
2025-11-19 10:00:52 +0100

    [Update] constructor DataRetrievalServiceImpl

--- System/DataServer/DataServer/src/main/java/via/sep3/dataserver/service/DataRetrievalServiceImpl.java
+++ System/DataServer/DataServer/src/main/java/via/sep3/dataserver/service/DataRetrievalServiceImpl.java
@@ -1,26 +1,22 @@
 package via.sep3.dataserver.service;
 
-import io.grpc.stub.StreamObserver;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.grpc.server.service.GrpcService;
 import org.springframework.stereotype.Service;
 
+import io.grpc.stub.StreamObserver;
 import via.sep3.dataserver.data.Course;
 import via.sep3.dataserver.data.CourseRepository;
+import via.sep3.dataserver.data.LearningStep;
+import via.sep3.dataserver.data.LearningStepId;
+import via.sep3.dataserver.data.LearningStepRepository;
 import via.sep3.dataserver.grpc.DataRetrievalServiceGrpc;
 import via.sep3.dataserver.grpc.GetCoursesRequest;
 import via.sep3.dataserver.grpc.GetCoursesResponse;
 import via.sep3.dataserver.grpc.GetLearningStepResponse;
-
-import org.springframework.grpc.server.service.GrpcService;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import via.sep3.dataserver.data.LearningStep;
-import via.sep3.dataserver.data.LearningStepId;
-import via.sep3.dataserver.data.LearningStepRepository;
 
 @GrpcService
 @Service
@@ -67,12 +63,11 @@ public class DataRetrievalServiceImpl extends DataRetrievalServiceGrpc.DataRetri
   public void getLearningStep(via.sep3.dataserver.grpc.GetLearningStepRequest request,
       StreamObserver<via.sep3.dataserver.grpc.GetLearningStepResponse> responseObserver) {
     var a = learningStepRepository.findById(
-        new LearningStepId(request.getCourseId(), request.getStepNumber()));
-    if(a.isEmpty())
-    {
+        new LearningStepId(request.getStepNumber(), request.getCourseId()));
+    if (a.isEmpty()) {
       responseObserver.onError(new RuntimeException("Learning step not found"));
       return;
-    }    
+    }
     LearningStep step = a.get();
     responseObserver.onNext(GetLearningStepResponse.newBuilder()
         .setLearningStep(convertToGrpcLearningStep(step))
