0c7951d3af6550d891f68f48bd4671d5e2f2cef6
Eduard Fekete <edko.fek@gmail.com>
2025-11-26 15:06:32 +0100

    [Fix] Logic server with old contract

--- System/LogicServer/gRPCRepo/gRPCLearningStepRepository.cs
+++ System/LogicServer/gRPCRepo/gRPCLearningStepRepository.cs
@@ -10,75 +10,42 @@ namespace gRPCRepo;
 
 public class gRPCLearningStepRepository(string host, int port, bool useTls = false) : gRPCRepository<Entities.LearningStep, (int, int)>(host, port, useTls)
 {
-    public override async Task<Entities.LearningStep> GetSingleAsync((int, int) id)
+    public override Task<Entities.LearningStep> AddAsync(Entities.LearningStep entity)
     {
-        var (courseId, stepOrder) = id;
-
-        var request = new LearningStepKey 
-        { 
-            CourseId = courseId, 
-            StepOrder = stepOrder 
-        };
-        
-        var response = await Client.GetLearningStepAsync(request) ?? throw new KeyNotFoundException($"LearningStep with CourseId {courseId} and StepOrder {stepOrder} not found.");
-        
-        return new Entities.LearningStep
-        {
-            CourseId = response.CourseId,
-            StepOrder = response.StepOrder,
-            Type = response.Type,
-            Content = response.Content
-        };
+        throw new NotImplementedException();
     }
-
-    public override async Task<Entities.LearningStep> AddAsync(Entities.LearningStep entity)
-    {
-        var protoObj = new via.sep3.dataserver.grpc.LearningStep
-        {
-            CourseId = entity.CourseId,
-            Type = entity.Type,
-            Content = entity.Content
-            // StepOrder is usually calculated by server or sent as 0
-        };
-
-        var response = await Client.AddLearningStepAsync(protoObj);
-
-        entity.StepOrder = response.StepOrder; // Server assigns the order
-        return entity;
-    }
-
-    public override async Task UpdateAsync(Entities.LearningStep entity)
-    {
-        var protoObj = new via.sep3.dataserver.grpc.LearningStep
-        {
-            CourseId = entity.CourseId,
-            StepOrder = entity.StepOrder,
-            Type = entity.Type,
-            Content = entity.Content
-        };
-
-        await Client.UpdateLearningStepAsync(protoObj);
-    }
-
-    public override async Task DeleteAsync((int, int) id)
-    {
-        var (courseId, stepOrder) = id;
-    {
-        var request = new LearningStepKey 
-        { 
-            CourseId = courseId, 
-            StepOrder = stepOrder 
-        };
-        await Client.DeleteLearningStepAsync(request);
-    }
-}
 
     public override Task ClearAsync()
     {
         throw new NotImplementedException();
     }
 
+    public override Task DeleteAsync((int, int) id)
+    {
+        throw new NotImplementedException();
+    }
+
     public override IQueryable<Entities.LearningStep> GetMany()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task<Entities.LearningStep> GetSingleAsync((int, int) id)
+    {
+        var (courseId, stepOrder) = id;
+        
+        var response = await Client.GetLearningStepAsync(new GetLearningStepRequest { CourseId = courseId, StepNumber = stepOrder }) ?? throw new KeyNotFoundException($"LearningStep with CourseId {courseId} and StepOrder {stepOrder} not found.");
+        
+        return new Entities.LearningStep
+        {
+            CourseId = response.LearningStep.CourseId,
+            StepOrder = response.LearningStep.StepOrder,
+            Type = response.LearningStep.Type,
+            Content = response.LearningStep.Content
+        };
+    }
+
+    public override Task UpdateAsync(Entities.LearningStep entity)
     {
         throw new NotImplementedException();
     }
