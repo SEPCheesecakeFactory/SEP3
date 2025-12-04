1814f5fee533a6062bd6a00ebdaac5c177a5c9b1
Gromykko <alswatchs@yahoo.com>
2025-12-04 11:12:34 +0100

    [upd] dataserver

--- System/DataServer/DataServer/src/main/java/via/sep3/dataserver/data/SystemUserRepository.java
+++ System/DataServer/DataServer/src/main/java/via/sep3/dataserver/data/SystemUserRepository.java
@@ -1,8 +1,16 @@
 package via.sep3.dataserver.data;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SystemUserRepository extends JpaRepository<SystemUser, Integer>
 {
   SystemUser getSystemUserById(Integer id);
+
+  @Query("SELECT u.username as username, SUM(p.currentStep * 10) as totalScore " +
+           "FROM SystemUser u JOIN UserCourseProgress p ON u.id = p.systemUser.id " +
+           "GROUP BY u.username " +
+           "ORDER BY totalScore DESC")
+    List<LeaderboardEntry> findTopPlayers(Pageable pageable);
 }
