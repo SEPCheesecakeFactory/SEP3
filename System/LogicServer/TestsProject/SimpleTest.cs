d4dc5d017a9f0e99e2225715da6dbfcddf9cd179
Eduard Fekete <edko.fek@gmail.com>
2025-12-10 16:05:51 +0100

    [Add] XUnit for logic server

---
+++ System/LogicServer/TestsProject/SimpleTest.cs
@@ -0,0 +1,16 @@
+﻿namespace TestsProject;
+
+public class SimpleTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var result = Helper();
+        Assert.True(result);
+    }
+
+    private bool Helper()
+    {
+        return true;
+    }
+}
