e4dcb812e00217667ef37d2a192be8d0b10db882
Eduard Fekete <edko.fek@gmail.com>
2025-12-12 13:45:43 +0100

    [Refactor] User Service - crud service + optional

--- System/ClientApp/BlazorApp/Services/IUserService.cs
+++ System/ClientApp/BlazorApp/Services/IUserService.cs
@@ -1,11 +1,12 @@
 using System;
 using BlazorApp.Entities;
+using BlazorApp.Shared;
 
 namespace BlazorApp.Services;
 
 public interface IUserService
 {
     Task UpdateUser(int id, User User);
-    public Task<List<User>> GetUsers();
-    Task<User> GetUser(int id);
+    public Task<Optional<List<User>>> GetUsers();
+    Task<Optional<User>> GetUser(int id);
 }
